b8ed8ac93a90d220d4b74df0696ddfd403d9ca35
EC2 Default User <ec2-user@ip-172-31-34-173.us-west-2.compute.internal>
2016-12-02 16:23:51 +0000

    minor change on SVM.R

--- code/SVM.R
+++ code/SVM.R
@@ -1,3 +1,6 @@
 svm1 <- read.csv(file.choose(), header = TRUE, stringsAsFactors = FALSE)
-svm_tune <- tune(svm1, train.x=svm1[,-ncol(svm1)], train.y=svm1[,ncol(svm1)], scale = FALSE, kernel="linear", ranges=list(cost=c(.1,1,10), gamma=c(.5,1,2)), tunecontrol = tune.control(cross = 5)
+svm_tune <- tune(svm1, train.x=svm1[,-ncol(svm1)],
+ train.y=as.factor(svm1[,ncol(svm1)]), scale = FALSE, kernel="linear",
+ ranges=list(cost=c(.1,1,10), gamma=c(.5,1,2)),
+ tunecontrol = tune.control(cross = 5))
 print(svm_tune)
